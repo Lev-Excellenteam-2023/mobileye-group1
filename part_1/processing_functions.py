3f7414c8eb82d58e880b3fc1f19955931448fa1b
Yisrael jacob <yisraeljakob@gmail.com>
2023-08-07 10:23:43 +0300

    Documentation of processing_functions file

--- part_1/processing_functions.py
+++ part_1/processing_functions.py
@@ -18,30 +18,38 @@ GREEN_X_COORDINATES = List[int]
 GREEN_Y_COORDINATES = List[int]
 
 
-def preprocess_image(c_image: np.ndarray) -> np.ndarray:
-   pass
-
-
-def gaussian_kernel_3d(kernel_size, sigma):
-    x, y, z = np.mgrid[-kernel_size // 2 + 1: kernel_size // 2 + 1, -kernel_size // 2 + 1: kernel_size // 2 + 1,
-              -kernel_size // 2 + 1: kernel_size // 2 + 1]
-    g = np.exp(-(x ** 2 + y ** 2 + z ** 2) / (2.0 * sigma ** 2))
-    return g / g.sum()
-
 def gaussian_kernel_2d(kernel_size, sigma):
+    """
+    Generate a 2D Gaussian kernel.
+
+    :param kernel_size: Size of the kernel.
+    :param sigma: Standard deviation of the Gaussian distribution.
+    :return: 2D Gaussian kernel.
+    """
     x, y = np.mgrid[-kernel_size // 2 + 1: kernel_size // 2 + 1, -kernel_size // 2 + 1: kernel_size // 2 + 1]
     g = np.exp(-(x ** 2 + y ** 2) / (2.0 * sigma ** 2))
     return g / g.sum()
 
 
 def gaussian_blur(image: np.ndarray) -> np.ndarray:
+    """
+    Apply Gaussian blur to an input image.
+
+    :param image: Input image as a numpy array.
+    :return: Blurred image as a numpy array.
+    """
     blurred_image = sg.convolve(image, gaussian_kernel_2d(7, 1), mode='same', method='fft')
 
     return blurred_image
 
 
-
 def find_red_coordinates(c_image: np.ndarray) -> Tuple[RED_X_COORDINATES, RED_Y_COORDINATES]:
+    """
+    Find coordinates of dominant red regions in an image.
+
+    :param c_image: Input RGB image as a numpy array.
+    :return: Tuple containing red x-coordinates and y-coordinates.
+    """
     c_image = np.uint8(c_image * 255)
     r_image = c_image[:, :, 0]
     g_image = c_image[:, :, 1]
@@ -51,39 +59,64 @@ def find_red_coordinates(c_image: np.ndarray) -> Tuple[RED_X_COORDINATES, RED_Y_
     blurred_image = gaussian_blur(red_image)
 
     return blurred_image / 255
-def find_green_coordinates(c_image: np.ndarray) -> np.ndarray:
-
-    r_image = c_image[:, :, 0]
-    g_image = c_image[:, :, 1]
-    b_image = c_image[:, :, 2]
 
 
 def find_green_coordinates(c_image: np.ndarray) -> Tuple[GREEN_X_COORDINATES, GREEN_Y_COORDINATES]:
+    """
+    Find coordinates of dominant green regions in an image.
+
+    :param c_image: Input RGB image as a numpy array.
+    :return: Tuple containing green x-coordinates and y-coordinates.
+    """
     c_image = np.uint8(c_image * 255)
     r_image = c_image[:, :, 0]
     g_image = c_image[:, :, 1]
     b_image = c_image[:, :, 2]
     green_image = g_image + b_image / 2 - r_image
 
-    blurred_image = gaussian_blur(green_image)
+    kernel = np.array([[1 / 25, 1 / 25, 1 / 25, 1 / 25, 1 / 25],
+                       [1 / 25, 1 / 25, 1 / 25, 1 / 25, 1 / 25],
+                       [1 / 25, 1 / 25, 1 / 25, 1 / 25, 1 / 25],
+                       [1 / 25, 1 / 25, 1 / 25, 1 / 25, 1 / 25],
+                       [1 / 25, 1 / 25, 1 / 25, 1 / 25, 1 / 25]])
+
+    blurred_image = sg.convolve(green_image, kernel, mode='same', method='direct')
+    # blurred_image = gaussian_blur(green_image)
 
     return blurred_image / 255
 
+# deprecated
+# def find_traffic_light_kernel() -> np.ndarray:
+#     kernel = Image.open("green_light_2.png")
+#     numpy_kernel = np.array(kernel)
+#
+#     return numpy_kernel
 
-def find_traffic_light_kernel() -> np.ndarray:
-    kernel = Image.open("green_light_2.png")
-    numpy_kernel = np.array(kernel)
-
-    return numpy_kernel
 
 def max_suppression(image: np.ndarray, min_value: float, kernel_size: int = 50) -> np.ndarray:
+    """
+    Apply non-maximum suppression to an input image.
+
+    :param image: Input image as a numpy array.
+    :param min_value: Minimum value for suppression.
+    :param kernel_size: Size of the maximum filter kernel.
+    :return: Suppressed image as a numpy array.
+    """
     max_image = maximum_filter(image, size=kernel_size, mode='constant')
     values = compare_max_supression(image, max_image, min_value)
     return values
 
-def compare_max_supression(image: np.ndarray, max_image: np.ndarray, min_value: float) -> Tuple[RED_X_COORDINATES, RED_Y_COORDINATES, GREEN_X_COORDINATES, GREEN_Y_COORDINATES]:
 
-    # return pixels that have same value in both images
+def compare_max_supression(image: np.ndarray, max_image: np.ndarray, min_value: float) -> Tuple[
+    RED_X_COORDINATES, RED_Y_COORDINATES, GREEN_X_COORDINATES, GREEN_Y_COORDINATES]:
+    """
+    Compare an image with a maximum image and perform suppression based on a threshold.
+
+    :param image: Original input image as a numpy array.
+    :param max_image: Image obtained after applying the maximum filter.
+    :param min_value: Minimum value for suppression.
+    :return: Tuple containing x-coordinates and y-coordinates of suppressed points.
+    """
     x_coordinates = []
     y_coordinates = []
     for x in range(image.shape[0]):
@@ -91,11 +124,4 @@ def compare_max_supression(image: np.ndarray, max_image: np.ndarray, min_value: 
             if image[x][y] == max_image[x][y] and max_image[x][y] > min_value:
                 x_coordinates.append(x)
                 y_coordinates.append(y)
-    # see dots on image
-    #plt.imshow(image)
-    #plt.scatter(green_y_coordinates, green_x_coordinates, s=20, color = 'green', marker='x')
-    #plt.show()
-    #plt.clf()
     return x_coordinates, y_coordinates
-
-x
