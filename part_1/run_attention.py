e7b68ede611fed8b9ad99a65b8d895428a87329f
Yisrael jacob <yisraeljakob@gmail.com>
2023-08-08 16:24:39 +0300

    0.47

--- part_1/run_attention.py
+++ part_1/run_attention.py
@@ -47,7 +47,7 @@ def find_tfl_lights(c_image: np.ndarray, **kwargs) -> Dict[str, Any]:
     # plt.show()
     # plt.imshow(green_image)
     # plt.show()
-    red_values = processing_functions.max_suppression(red_image, 047, kernel_size =  90)
+    red_values = processing_functions.max_suppression(red_image, 0.47, kernel_size =  90)
     #red_values = processing_functions.filter_red_points(c_image, red_values)
     green_values = processing_functions.max_suppression(green_image, 0.668)
     green_values = processing_functions.filter_green_points(c_image, green_values)
