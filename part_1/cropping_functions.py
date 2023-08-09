e8622055827b651f37765aeb3e2ed3609b613803
yehuda shani <yehudashani88@gmail.com>
2023-08-09 09:48:56 +0300

    added find contours with cv2

--- part_1/cropping_functions.py
+++ part_1/cropping_functions.py
@@ -1,6 +1,7 @@
 import pandas as pd
 import numpy as np
 import sklearn.cluster
+import cv2
 import matplotlib.pyplot as plt
 
 
@@ -11,22 +12,38 @@ def find_center_and_radius(cropped_image):
     :param cropped_image: Input cropped image as a numpy array.
     :return: Tuple containing center and radius of the circle.
     """
-    cropped_image_1d = cropped_image.reshape(-1, 1)
-
-    num_clusters = 2
-    kmeans = sklearn.cluster.KMeans(n_clusters=num_clusters, random_state=0).fit(cropped_image_1d)
-    cluster_centers = kmeans.cluster_centers_
-    labels = kmeans.labels_
-    plt.scatter(cropped_image_1d, labels, c=labels, cmap='rainbow')
-    plt.scatter(cluster_centers, np.arange(num_clusters), marker='x', s=200, linewidths=3, color='black')
-    plt.show()
-
-    # Choose the cluster with the highest amount of points
-    cluster = np.argmin(np.bincount(labels))
+    # cropped_image_1d = cropped_image.reshape(-1, 1)
+    #
+    # num_clusters = 2
+    # kmeans = sklearn.cluster.KMeans(n_clusters=num_clusters, random_state=0).fit(cropped_image_1d)
+    # cluster_centers = kmeans.cluster_centers_
+    # labels = kmeans.labels_
+    # plt.scatter(cropped_image_1d, labels, c=labels, cmap='rainbow')
+    # plt.scatter(cluster_centers, np.arange(num_clusters), marker='x', s=200, linewidths=3, color='black')
+    # plt.show()
+    #
+    # # Choose the cluster with the highest amount of points
+    # cluster = np.argmin(np.bincount(labels))
+    #
+    # # Find the center and radius of the circle
+    # # sum of all points in the cluster divided by the number of points in the cluster
+    # center = np.sum(cluster_centers[cluster]) / len(cluster_centers[cluster])
 
     # Find the center and radius of the circle
-    # sum of all points in the cluster divided by the number of points in the cluster
-    center = np.sum(cluster_centers[cluster]) / len(cluster_centers[cluster])
+    contoured_image = np.uint8(cropped_image * 255)
+    contours, hierarchy = cv2.findContours(contoured_image, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+
+    contoured_canvas = np.zeros_like(contoured_image, dtype=np.uint8)
+    cv2.drawContours(contoured_canvas, contours, -1, (255, 255, 255), 1)  # Draw all contours in white
+
+    # Display the image with contours using plt.imshow()
+    plt.imshow(contoured_canvas, cmap='gray')  # 'gray' colormap for grayscale images
+
+    # Add a title if desired
+    plt.title("Image with Contours")
+
+    # Show the plot
+    plt.show()
 
 
 def big_crop(image: np.ndarray, value:tuple, color: str) -> np.ndarray:
@@ -54,5 +71,7 @@ def big_crop(image: np.ndarray, value:tuple, color: str) -> np.ndarray:
         min_y_value = max(value[0] - 80, 0)
     else:
         raise Exception("Invalid color. Expected 'r' or 'g'.")
+
+
     return image[min_y_value:max_y_value, min_x_value:max_x_value]
 
